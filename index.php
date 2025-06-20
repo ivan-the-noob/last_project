56da48ea72e37faa0245df5324b120c30177f0ba
Ivan <84211523+ivan-the-noob@users.noreply.github.com>
2025-06-20 14:53:52 +0800

    a

--- index.php
+++ index.php
@@ -202,8 +202,7 @@ $conn->close();
 
                                 </div>
                                  <div class="dropdown second-dropdown">
-                                <button class="btn" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
-                              <img src="assets/img/<?php echo htmlspecialchars($profile_picture); ?>" alt="Profile Image" class="profile">
+                                <button class="btn" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">  <img src="assets/img/<?php echo htmlspecialchars($profile_picture); ?>" alt="Profile Image" class="profile">
                                 </button>
                                 <ul class="dropdown-menu custom-center-dropdown" aria-labelledby="dropdownMenuButton2">
                                     <li><a class="dropdown-item" href="features/users/web/api/dashboard.php">Profile</a></li>
